1da5b8715a3e88518069ae34560ba9cf0654e16c
Filipe Almeida <filipe_guimaraes@outlook.com>
2021-08-28 02:41:05 -0300

    Updating

---
+++ aws-lambda/lambda-stop/stop_ec2.py
@@ -0,0 +1,5 @@
+import boto3
+ec2 = boto3.resource('ec2')
+def lambda_handler(event, context):
+    filters = [{'Name': 'tag-key', 'Values': ['Desliga']}]
+    ec2.instances.filter(Filters=filters).stop()
