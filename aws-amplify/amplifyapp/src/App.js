b173aca3ac3ffb54d9d91ebe53ea20e96df92e6d
Filipe Almeida <filipe_guimaraes@outlook.com>
2021-09-04 04:27:32 -0300

    Updating Amplify

--- aws-amplify/amplifyapp/src/App.js
+++ aws-amplify/amplifyapp/src/App.js
@@ -8,6 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+	  THis is a test message
         </p>
         <a
           className="App-link"
